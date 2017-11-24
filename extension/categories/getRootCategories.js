6abf0314bdf447fc29de31f3b42e33aa41ed716e
Timo Ebel <timo.ebel@shopgate.com>
2017-11-24 16:57:14 +0100

    BIGC-498 Extracted the class into its own file. Moved instantiation outside the callbacks pipeline as it's stateless and only needs to be instanciated once on startup.

--- extension/categories/getRootCategories.js
+++ extension/categories/getRootCategories.js
@@ -1,50 +1,24 @@
 const BigCommerce = require('node-bigcommerce')
+const BigCommerceCategoryApi = require('./classes/BigCommerceCategoryApi.js')
 
-class BigCommerceCategoryApi {
-  constructor (apiVersion2Client, apiVersion3Client) {
-    this.apiVersion2Client = apiVersion2Client
-    this.apiVersion3Client = apiVersion3Client
-  }
-
-  getRootCategories () {
-    let resultCategories = []
-
-    return this.apiVersion3Client.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url').then((firstPage) => {
-      let pagePromises = [firstPage]
-
-      if (firstPage.meta.pagination.total_pages > 1) {
-        for (let i = 2; i <= firstPage.meta.pagination.total_pages; i++) {
-          pagePromises.push(this.apiVersion3Client.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url&page=' + i))
-        }
-      }
-
-      return Promise.all(pagePromises)
-    }).then((allPages) => {
-      let productCountPerCategoryPromises = []
-
-      allPages.forEach((page) => {
-        page.data.forEach((bigCommerceCategory) => {
-          resultCategories.push({
-            id: bigCommerceCategory.id,
-            name: bigCommerceCategory.name,
-            productCount: 0,
-            imageUrl: bigCommerceCategory.image_url
-          })
-
-          productCountPerCategoryPromises.push(this.apiVersion2Client.get('/products/count?category=' + encodeURIComponent(bigCommerceCategory.name)))
-        })
-      })
-
-      return Promise.all(productCountPerCategoryPromises)
-    }).then((productCounts) => {
-      productCounts.forEach((productCount, resultCategoryIndex) => {
-        resultCategories[resultCategoryIndex].productCount = productCount.count
-      })
-
-      return resultCategories
-    })
-  }
-}
+const bigCommerceCategoryApi = new BigCommerceCategoryApi(
+  new BigCommerce({
+    logLevel: 'info',
+    clientId: '***',
+    accessToken: '***',
+    storeHash: '***',
+    responseType: 'json',
+    apiVersion: 'v2'
+  }),
+  new BigCommerce({
+    logLevel: 'info',
+    clientId: '***',
+    accessToken: '***',
+    storeHash: '***',
+    responseType: 'json',
+    apiVersion: 'v3'
+  })
+)
 
 /**
  * @param {object} context
@@ -52,26 +26,7 @@ class BigCommerceCategoryApi {
  * @param {Function} cb
  */
 module.exports = function (context, input, cb) {
-  let apiWrapper = new BigCommerceCategoryApi(
-    new BigCommerce({
-      logLevel: 'info',
-      clientId: '***',
-      accessToken: '***',
-      storeHash: '***',
-      responseType: 'json',
-      apiVersion: 'v2'
-    }),
-    new BigCommerce({
-      logLevel: 'info',
-      clientId: '***',
-      accessToken: '***',
-      storeHash: '***',
-      responseType: 'json',
-      apiVersion: 'v3'
-    })
-  )
-
-  apiWrapper.getRootCategories().then((categories) => {
+  bigCommerceCategoryApi.getRootCategories().then((categories) => {
     cb(null, {categories: categories})
   }).catch(console.err)
 }
