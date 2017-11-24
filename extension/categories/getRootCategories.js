5180f47c26ef261090263f4c3ae90ba11eb2a972
Timo Ebel <timo.ebel@shopgate.com>
2017-11-24 14:46:30 +0100

    BIGC-498 Added a wrapper class for API communication and implemented fetching root categories there (no product count, yet).

--- extension/categories/getRootCategories.js
+++ extension/categories/getRootCategories.js
@@ -1,5 +1,42 @@
 const BigCommerce = require('node-bigcommerce')
 
+class BigCommerceCategoryApi {
+  constructor (apiVersion2Client, apiVersion3Client) {
+    this.apiVersion2Client = apiVersion2Client
+    this.apiVersion3Client = apiVersion3Client
+  }
+
+  getRootCategories () {
+    let resultCategories = []
+
+    return this.apiVersion3Client.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url').then((firstPage) => {
+      let promises = [firstPage]
+
+      if (firstPage.meta.pagination.total_pages > 1) {
+        for (let i = 2; i <= firstPage.meta.pagination.total_pages; i++) {
+          promises.push(this.apiVersion3Client.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url&limit=1&page=' + i))
+        }
+      }
+
+      return Promise.all(promises)
+    }).then((allPages) => {
+      allPages.forEach((page) => {
+        page.data.forEach((bigCommerceCategory) => {
+          resultCategories.push({
+            id: bigCommerceCategory.id,
+            name: bigCommerceCategory.name,
+            productCount: 0,
+            imageUrl: bigCommerceCategory.image_url
+          })
+        })
+      })
+
+      console.log(resultCategories)
+      return resultCategories
+    })
+  }
+}
+
 /**
  * @param {object} context
  * @param {object} input - Properties depend on the pipeline this is used for
