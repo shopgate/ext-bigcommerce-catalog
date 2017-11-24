8bea5532d50e36d09d022d7a7523c52fe5a8678c
Timo Ebel <timo.ebel@shopgate.com>
2017-11-24 11:22:21 +0100

    BIGC-498 Experimenting with promises depending on other promises and trying to find a way to structure those in a readable way. Still not happy, but I guess it's a start.

--- extension/categories/getRootCategories.js
+++ extension/categories/getRootCategories.js
@@ -6,33 +6,55 @@ const BigCommerce = require('node-bigcommerce')
  * @param {Function} cb
  */
 module.exports = function (context, input, cb) {
-  const bigCommerce = new BigCommerce({
+  let resultCategories = []
+
+  const bigCommerceV2 = new BigCommerce({
     logLevel: 'info',
-    clientId: '***',
-    accessToken: '***',
-    storeHash: '***',
+    clientId: '5qsw38039y6dwq37wp6nzabyq11cpru',
+    accessToken: 'evgf1d16l0iu1bpmckjw8an0wkxl9hx',
+    storeHash: 'r5s844ad',
+    responseType: 'json',
+    apiVersion: 'v2'
+  })
+
+  const bigCommerceV3 = new BigCommerce({
+    logLevel: 'info',
+    clientId: '5qsw38039y6dwq37wp6nzabyq11cpru',
+    accessToken: 'evgf1d16l0iu1bpmckjw8an0wkxl9hx',
+    storeHash: 'r5s844ad',
     responseType: 'json',
     apiVersion: 'v3'
   })
 
-  bigCommerce.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url').then((bigCommerceCategories) => {
-    console.log(bigCommerceCategories)
-    const resultCategories = []
+  bigCommerceV3.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url')
+    .then((bigCommerceCategories) => {
+      let productCountPromises = []
 
-    bigCommerceCategories.data.forEach((bigCommerceCategory) => {
-      resultCategories.push({
-        id: bigCommerceCategory.id,
-        name: bigCommerceCategory.name,
-        productCount: 0,
-        imageUrl: bigCommerceCategory.image_url
+      bigCommerceCategories.data.forEach((bigCommerceCategory) => {
+        resultCategories.push({
+          id: bigCommerceCategory.id,
+          name: bigCommerceCategory.name,
+          productCount: 0,
+          imageUrl: bigCommerceCategory.image_url
+        })
+
+        productCountPromises.push(bigCommerceV2.get('/products/count?category=' + encodeURIComponent(bigCommerceCategory.name)))
+      })
+
+      return Promise.all(new Map([
+        ['categories', bigCommerceCategories],
+        ['productCounts', Promise.all(productCountPromises)]
+      ]))
+    })
+    .then(async (promises) => {
+      await new Map(promises).get('productCounts').then((productCounts) => {
+        productCounts.forEach((promise, index) => {
+          resultCategories[index].productCount = promise.count
+        })
       })
     })
-
-    const totalPages = bigCommerceCategories.meta.pagination.total_pages
-    if (totalPages === 1) {
-      return cb(null, {categories: resultCategories})
-    } else {
-      return cb(null, {categories: resultCategories})
-    }
-  }, console.error)
+    .then(() => {
+      cb(null, {categories: resultCategories})
+    })
+    .catch(console.err)
 }
