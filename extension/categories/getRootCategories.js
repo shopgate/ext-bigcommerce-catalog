cbefbf3811b39da423a016120e4c8b2b8226967b
Timo Ebel <timo.ebel@shopgate.com>
2017-11-24 15:22:31 +0100

    BIGC-498 Clean up of the 'main' function.

--- extension/categories/getRootCategories.js
+++ extension/categories/getRootCategories.js
@@ -52,8 +52,6 @@ class BigCommerceCategoryApi {
  * @param {Function} cb
  */
 module.exports = function (context, input, cb) {
-  let resultCategories = []
-
   let apiWrapper = new BigCommerceCategoryApi(
     new BigCommerce({
       logLevel: 'info',
@@ -76,46 +74,4 @@ module.exports = function (context, input, cb) {
   apiWrapper.getRootCategories().then((categories) => {
     cb(null, {categories: categories})
   }).catch(console.err)
-  return
-
-  const bigCommerceV3 = new BigCommerce({
-    logLevel: 'info',
-    clientId: '5qsw38039y6dwq37wp6nzabyq11cpru',
-    accessToken: 'evgf1d16l0iu1bpmckjw8an0wkxl9hx',
-    storeHash: 'r5s844ad',
-    responseType: 'json',
-    apiVersion: 'v3'
-  })
-
-  bigCommerceV3.get('/catalog/categories?parent_id=0&is_visible=1&include_fields=id,name,image_url')
-    .then((bigCommerceCategories) => {
-      let productCountPromises = []
-
-      bigCommerceCategories.data.forEach((bigCommerceCategory) => {
-        resultCategories.push({
-          id: bigCommerceCategory.id,
-          name: bigCommerceCategory.name,
-          productCount: 0,
-          imageUrl: bigCommerceCategory.image_url
-        })
-
-        productCountPromises.push(bigCommerceV2.get('/products/count?category=' + encodeURIComponent(bigCommerceCategory.name)))
-      })
-
-      return Promise.all(new Map([
-        ['categories', bigCommerceCategories],
-        ['productCounts', Promise.all(productCountPromises)]
-      ]))
-    })
-    .then(async (promises) => {
-      await new Map(promises).get('productCounts').then((productCounts) => {
-        productCounts.forEach((promise, index) => {
-          resultCategories[index].productCount = promise.count
-        })
-      })
-    })
-    .then(() => {
-      cb(null, {categories: resultCategories})
-    })
-    .catch(console.err)
 }
