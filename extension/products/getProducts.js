47b3a9109053930155fdf21b64be3e1eed1c7c7a
Timo Ebel <timo.ebel@shopgate.com>
2017-11-27 16:12:52 +0100

    Revert "BIGC-489: Added first rough version of get products and added a mocha test to be able to execute code locally"
    This reverts commit 461c799f33d563e25123fcd064b545a0ef46e164.

--- extension/products/getProducts.js
+++ extension/products/getProducts.js
@@ -1,6 +1,3 @@
-const BigCommerce = require('node-bigcommerce')
-const BigCommerceProduct = require('./BigCommerceProduct.js')
-
 /**
  * @param {object} context
  * @param {object} input - Properties depend on the pipeline this is used for
@@ -17,52 +14,8 @@ const BigCommerceProduct = require('./BigCommerceProduct.js')
  * @param {Function} cb
  */
 module.exports = function (context, input, cb) {
-  const bigCommerceApi = new BigCommerce({
-    logLevel: 'info',
-    clientId: '5qsw38039y6dwq37wp6nzabyq11cpru',
-    accessToken: 'evgf1d16l0iu1bpmckjw8an0wkxl9hx',
-    responseType: 'json',
-    storeHash: 'r5s844ad',
-    apiVersion: 'v3' // Default is v2
-  })
-
-  let products = []
-
-  bigCommerceApi.get('/catalog/products?limit=1').then(async data => {
-    console.log(data)
-
-    for (let bigCommerceProductData of data['data']) {
-      /* @type {BigCommerceProduct} */
-      const bigCommerceProduct = new BigCommerceProduct(bigCommerceApi, bigCommerceProductData)
-
-      await bigCommerceProduct.getBrand().then((brand) => {
-        products.push({
-          id: bigCommerceProduct.getId(),
-          active: bigCommerceProduct.isActive(),
-          availability: bigCommerceProduct.getAvailablity(),
-          identifiers: bigCommerceProduct.getIdentifiers(),
-          manufacturer: brand,
-          name: bigCommerceProduct.getName(),
-          stock: bigCommerceProduct.getStock(),
-          rating: bigCommerceProduct.getRating(),
-          featuredImageUrl: bigCommerceProduct.getFeaturedImageUrl(),
-          price: bigCommerceProduct.getPrice(),
-          flags: bigCommerceProduct.getTags(),
-          liveshoppings: [],
-          highlight: bigCommerceProduct.getHighlight(),
-          parent: bigCommerceProduct.getParent(),
-          type: bigCommerceProduct.getType(),
-          tags: bigCommerceProduct.getTags()
-        })
-      }
-      )
-    }
-
-    console.log(products)
-
-    cb(null, {
-      totalProductCount: 0,
-      products: products
-    })
+  cb(null, {
+    totalProductCount: 0,
+    products: []
   })
 }
