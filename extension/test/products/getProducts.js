7c133f8a01a63e05ec864cfff57bf14275a4185a
Oliver Müller <oliver.mueller@shopgate.com>
2017-12-04 12:29:51 +0100

    BIGC-533: Deleted empty/unecessary test file

--- extension/test/products/getProducts.js
+++
@@ -1,8 +0,0 @@
-const getProducts = require('../../products/getProducts.js')
-
-describe('call get products', function () {
-  let context = ''
-  let input = {}
-  let cb = function () {}
-  getProducts(context, input, cb)
-})
