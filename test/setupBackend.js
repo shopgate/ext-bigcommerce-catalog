e0c865ecf53360b5fe61da11256355ae95d4b839
Oliver Müller <oliver.mueller@shopgate.com>
2017-12-07 09:10:27 +0100

    BIGC-511: added logic to create .integration-credentials.js file for integration tests

--- test/setupBackend.js
+++ test/setupBackend.js
@@ -1,12 +1,23 @@
 const Module = require('module')
 const path = require('path')
 const process = require('process')
+const fs = require('fs')
 
 const extensionModulesPath = path.dirname(__dirname) + '/extension/node_modules'
+const integrationTestCredentialFile = path.dirname(__dirname) + '/.integration-credentials.js'
 
 process.env['NODE_PATH'] = extensionModulesPath
 process.env['JUNIT_REPORT_PATH'] = '../build/mocha.xml'
 process.env['JUNIT_REPORT_STACK'] = 1
 
-Module._initPaths()
+fs.writeFile(integrationTestCredentialFile, 'module.exports = {\n' +
+  '  clientId: \'***\',\n' +
+  '  accessToken: \'***\',\n' +
+  '  storeHash: \'***\'\n' +
+  '}', {flag: 'wx'}, function (err) {
+  if (err) {
+    return true
+  }
+})
 
+Module._initPaths()
