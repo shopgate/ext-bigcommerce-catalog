2560cbcc30b10d6e3d1ba8971f8ff5bb41190fba
Timo Ebel <timo.ebel@shopgate.com>
2017-12-08 17:13:41 +0100

    Adapted unit test to use the factory classes as well.

--- test/integration/extension/lib/catalog/category/Repository/BigcommerceCategory.js
+++ test/integration/extension/lib/catalog/category/Repository/BigcommerceCategory.js
@@ -1,8 +1,7 @@
 const IntegrationCredentials = require('../../../../../../../.integration-credentials')
-const BigCommerce = require('node-bigcommerce')
 const BigcommerceCategory = require('../../../../../../../extension/lib/catalog/category/Repository/BigcommerceCategory.js')
-const GetAllVisibleCategoriesByParentId = require('../../../../../../../extension/lib/catalog/category/Repository/Command/GetAllVisibleCategoriesByParentId.js')
-const GetProductCountsByCategoryIds = require('../../../../../../../extension/lib/catalog/category/Repository/Command/GetProductCountsByCategoryIds.js')
+const BigCommerceFactory = require('../../../../../../../extension/lib/steps/BigCommerceFactory.js')
+const BigcommerceRepositoryCommand = require('../../../../../../../extension/lib/catalog/category/Factory/BigcommerceRepositoryCommand')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const chaiSubset = require('chai-subset')
@@ -13,29 +12,17 @@ chai
   .should()
 
 describe('BigcommerceCategoryTest', function () {
-  const bigCommerceApiVersion3 = new BigCommerce({
-    logLevel: 'info',
-    clientId: IntegrationCredentials.clientId,
-    accessToken: IntegrationCredentials.accessToken,
-    storeHash: IntegrationCredentials.storeHash,
-    responseType: 'json',
-    apiVersion: 'v3'
-  })
-
-  const bigCommerceApiVersion2 = new BigCommerce({
-    logLevel: 'info',
-    clientId: IntegrationCredentials.clientId,
-    accessToken: IntegrationCredentials.accessToken,
-    storeHash: IntegrationCredentials.storeHash,
-    responseType: 'json',
-    apiVersion: 'v2'
-  })
-  const getAllVisibleCategoriesByParentIdCommand = new GetAllVisibleCategoriesByParentId(bigCommerceApiVersion3)
-  const getProductCountsByCategoryIds = new GetProductCountsByCategoryIds(bigCommerceApiVersion2)
+  const bigcommerceCategoryRepositoryCommandFactory = new BigcommerceRepositoryCommand(
+    new BigCommerceFactory(
+      IntegrationCredentials.clientId,
+      IntegrationCredentials.accessToken,
+      IntegrationCredentials.storeHash
+    )
+  )
 
   describe('#getRootCategories', function () {
     it('calls GetAllVisibleCategoriesByParentId#execute', function () {
-      let subjectUnderTest = new BigcommerceCategory(getAllVisibleCategoriesByParentIdCommand, null, getProductCountsByCategoryIds)
+      let subjectUnderTest = new BigcommerceCategory(bigcommerceCategoryRepositoryCommandFactory)
 
       return subjectUnderTest.getRootCategories().should.eventually.containSubset([
         {
