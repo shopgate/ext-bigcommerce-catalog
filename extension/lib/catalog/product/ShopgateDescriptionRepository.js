138dc4374286a85d3e2e5ad76ef18aea07ae0bbf
Aaron Martin <aaron.martin@shopgate.com>
2017-12-14 08:32:27 -0600

    BIGC-509: naming change apiVersion3Client to _apiVersion3Client in ShopgateProductDescriptionRepository class

--- extension/lib/catalog/product/ShopgateDescriptionRepository.js
+++ extension/lib/catalog/product/ShopgateDescriptionRepository.js
@@ -3,13 +3,14 @@ class ShopgateProductDescriptionRepository {
    * @param {BigCommerce} apiVersion3Client
    */
   constructor (apiVersion3Client) {
-    this.apiVersion3Client = apiVersion3Client
+    this._apiVersion3Client = apiVersion3Client
   }
 
   async get (productId) {
-    const bigCommerceProductResponse = await this.apiVersion3Client.get(
+    const bigCommerceProductResponse = await this._apiVersion3Client.get(
       '/catalog/products/' + productId + '?include_fields=description')
     return bigCommerceProductResponse.data.description
   }
 }
+
 module.exports = ShopgateProductDescriptionRepository
