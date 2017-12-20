bd2bac32010ae869050353a3041530fdd20170bf
Sofija Blazevski Rulani <sofija.blazevski@shopgate.com>
2017-12-20 11:18:50 +0100

    BIGC-510 Renamed local variable.

--- extension/lib/catalog/product/ShopgateShippingRepository.js
+++ extension/lib/catalog/product/ShopgateShippingRepository.js
@@ -16,11 +16,11 @@ class ShopgateShippingRepository {
    */
   async get (productId) {
     const response = await this._client.get('/catalog/products/' + productId + '?include_fields=fixed_cost_shipping_price,is_free_shipping')
-    const productRepository = this._productEntityFactory.create(response.data)
+    const productEntity = this._productEntityFactory.create(response.data)
 
     return new ShopgateProductShipping(
-      productRepository.getShippingCost(),
-      await productRepository.getShippingCostCurrency()
+      productEntity.getShippingCost(),
+      await productEntity.getShippingCostCurrency()
     )
   }
 }
