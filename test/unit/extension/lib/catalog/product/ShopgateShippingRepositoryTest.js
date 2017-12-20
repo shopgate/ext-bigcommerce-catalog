565a612d5f4390c48145b8a9b4b5767efadec00d
Sofija Blazevski Rulani <sofija.blazevski@shopgate.com>
2017-12-20 15:35:45 +0100

    BIGC-510 Avoiding inspection issues.

--- test/unit/extension/lib/catalog/product/ShopgateShippingRepositoryTest.js
+++ test/unit/extension/lib/catalog/product/ShopgateShippingRepositoryTest.js
@@ -9,6 +9,7 @@ const BigCommerceConfiguration = require('./../../../../../../extension/lib/stor
 describe('ShopgateProductShippingRepositoryTest', () => {
   let clientStub
   let bigCommerceProductEntityFactoryStub, bigCommerceConfigurationStub
+
   beforeEach(() => {
     clientStub = sinon.createStubInstance(BigCommerceClient)
     clientStub.get.returns({ data: {} })
@@ -18,7 +19,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
   })
 
   it('should provide free shipping when big commerce product has is_free_shipping set', () => {
-    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity({is_free_shipping: 1}, bigCommerceConfigurationStub))
+    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 1}), bigCommerceConfigurationStub))
     const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
@@ -26,7 +27,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
   })
 
   it('should provide free shipping when big commerce product has is_free_shipping set even though there is fixed_cost_shipping_price', () => {
-    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity({is_free_shipping: 1, fixed_cost_shipping_price: 5.59}, bigCommerceConfigurationStub))
+    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 1, fixed_cost_shipping_price: 5.59}), bigCommerceConfigurationStub))
     const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
@@ -34,7 +35,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
   })
 
   it('should provide free shipping when there is no free shipping available but the shipping price is 0', () => {
-    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity({is_free_shipping: 0, fixed_cost_shipping_price: 0}, bigCommerceConfigurationStub))
+    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 0, fixed_cost_shipping_price: 0}), bigCommerceConfigurationStub))
     const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
@@ -42,10 +43,19 @@ describe('ShopgateProductShippingRepositoryTest', () => {
   })
 
   it('should provide shipping price when there is no free shipping available', () => {
-    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity({is_free_shipping: 0, fixed_cost_shipping_price: 5.59}, bigCommerceConfigurationStub))
+    bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 0, fixed_cost_shipping_price: 5.59}), bigCommerceConfigurationStub))
     const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
     const expectedShipping = new ShopgateProductShipping(5.59, 'USD')
 
     return subjectUnderTest.get(1).should.eventually.deep.equal(expectedShipping)
   })
+
+  /**
+   * A solution to avoid having invalid inspection due to some js docs problems.
+   * @param {*} bigCommerceProduct
+   * @returns {*}
+   */
+  this._createBigCommerceProduct = (bigCommerceProduct) => {
+    return bigCommerceProduct
+  }
 })
