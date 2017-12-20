36735ac16a254effa6e072b62ea082252c758678
Aaron Martin <aaron.martin@shopgate.com>
2017-12-20 10:25:42 -0600

    BIGC-514: removed identifiers from propertiey repository

--- extension/lib/catalog/product/ShopgatePropertiesRepository.js
+++ extension/lib/catalog/product/ShopgatePropertiesRepository.js
@@ -1,6 +1,5 @@
 const ProductWeight = require('./property/ShopgateWeight')
 const PropertyMapper = require('./ShopgatePropertyMapper')
-const ShopgateProperty = require('./ShopgateProperty')
 
 class ShopgatePropertiesRepository {
   /**
@@ -14,23 +13,14 @@ class ShopgatePropertiesRepository {
    * @param {number} id
    * @returns {Promise<ShopgateProperty[]>}
    */
-  async get (id) {
-    const response = await this._client.get('/catalog/products/' + id + '?include_fields=weight,upc,mpn,gtin,brand_id')
+  async get (id) {gi
+    const response = await this._client.get('/catalog/products/' + id + '?include_fields=weight')
     const shopgateProperties = []
-    const allowedIdentifiers = ['upc', 'mpn', 'gtin']
     Array.prototype.push.apply(
       shopgateProperties,
       PropertyMapper.mapWeight(new ProductWeight(response.data.weight, 'g'))
     )
 
-    allowedIdentifiers.forEach(function (identifier) {
-      if (response.data[identifier]) {
-        shopgateProperties.push(
-          new ShopgateProperty(identifier.toUpperCase(),
-            response.data[identifier]),
-        )
-      }
-    })
     return shopgateProperties
   }
 }
