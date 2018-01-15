5a0b56cc44b1c5ed686d66294a0612d2fcc85f1d
Aaron Martin <aaron.martin@shopgate.com>
2018-01-15 07:16:13 -0600

    BIGC-548: Updated ShopgateShippingRepositoryTest to include storeLogger

--- test/unit/extension/lib/catalog/product/ShopgateShippingRepositoryTest.js
+++ test/unit/extension/lib/catalog/product/ShopgateShippingRepositoryTest.js
@@ -5,10 +5,11 @@ const BigCommerceProductEntityFactory = require('../../../../../../extension/lib
 const ShopgateProductShipping = require('../../../../../../extension/lib/catalog/product/entity/ShopgateShipping')
 const BigCommerceProductEntity = require('./../../../../../../extension/lib/catalog/product/entity/BigCommerceProduct')
 const BigCommerceConfiguration = require('./../../../../../../extension/lib/store/configuration/BigCommerceRepository')
+const StoreLogger = require('../../../../../../extension/lib/store/logger/StoreLogger')
 
 describe('ShopgateProductShippingRepositoryTest', () => {
   let clientStub
-  let bigCommerceProductEntityFactoryStub, bigCommerceConfigurationStub
+  let bigCommerceProductEntityFactoryStub, bigCommerceConfigurationStub, storeLogger
 
   beforeEach(() => {
     clientStub = sinon.createStubInstance(BigCommerceClient)
@@ -16,11 +17,12 @@ describe('ShopgateProductShippingRepositoryTest', () => {
     bigCommerceProductEntityFactoryStub = sinon.createStubInstance(BigCommerceProductEntityFactory)
     bigCommerceConfigurationStub = sinon.createStubInstance(BigCommerceConfiguration)
     bigCommerceConfigurationStub.getCurrency.returns('USD')
+    storeLogger = sinon.createStubInstance(StoreLogger)
   })
 
   it('should provide free shipping when big commerce product has is_free_shipping set', () => {
     bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 1}), bigCommerceConfigurationStub))
-    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
+    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub, storeLogger)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
     return subjectUnderTest.get(1).should.eventually.deep.equal(expectedShipping)
@@ -28,7 +30,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
 
   it('should provide free shipping when big commerce product has is_free_shipping set even though there is fixed_cost_shipping_price', () => {
     bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 1, fixed_cost_shipping_price: 5.59}), bigCommerceConfigurationStub))
-    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
+    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub, storeLogger)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
     return subjectUnderTest.get(1).should.eventually.deep.equal(expectedShipping)
@@ -36,7 +38,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
 
   it('should provide free shipping when there is no free shipping available but the shipping price is 0', () => {
     bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 0, fixed_cost_shipping_price: 0}), bigCommerceConfigurationStub))
-    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
+    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub, storeLogger)
     const expectedShipping = new ShopgateProductShipping(0, 'USD')
 
     return subjectUnderTest.get(1).should.eventually.deep.equal(expectedShipping)
@@ -44,7 +46,7 @@ describe('ShopgateProductShippingRepositoryTest', () => {
 
   it('should provide shipping price when there is no free shipping available', () => {
     bigCommerceProductEntityFactoryStub.create.returns(new BigCommerceProductEntity(this._createBigCommerceProduct({is_free_shipping: 0, fixed_cost_shipping_price: 5.59}), bigCommerceConfigurationStub))
-    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub)
+    const subjectUnderTest = new ShopgateProductShippingRepository(clientStub, bigCommerceProductEntityFactoryStub, storeLogger)
     const expectedShipping = new ShopgateProductShipping(5.59, 'USD')
 
     return subjectUnderTest.get(1).should.eventually.deep.equal(expectedShipping)
