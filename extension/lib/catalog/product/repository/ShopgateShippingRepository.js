48b1cc04f969ae2fb2e6bcfa263cc4714371f5e4
Aaron Martin <aaron.martin@shopgate.com>
2018-01-15 07:14:09 -0600

    BIGC-618: added StoreLogger to ShopgateShippingRepository and added time logging

--- extension/lib/catalog/product/repository/ShopgateShippingRepository.js
+++ extension/lib/catalog/product/repository/ShopgateShippingRepository.js
@@ -4,10 +4,12 @@ class ShopgateShippingRepository {
   /**
    * @param {BigCommerce} bigCommerceApiV3Client
    * @param {BigCommerceProductEntityFactory} bigCommerceProductEntityFactory
+   * @param {StoreLogger} storeLogger
    */
-  constructor (bigCommerceApiV3Client, bigCommerceProductEntityFactory) {
+  constructor (bigCommerceApiV3Client, bigCommerceProductEntityFactory, storeLogger) {
     this._client = bigCommerceApiV3Client
     this._productEntityFactory = bigCommerceProductEntityFactory
+    this._storeLogger = storeLogger
   }
 
   /**
@@ -15,7 +17,9 @@ class ShopgateShippingRepository {
    * @returns {Promise<ShopgateProductShipping>}
    */
   async get (productId) {
+    this._storeLogger.startTimmer()
     const response = await this._client.get('/catalog/products/' + productId + '?include_fields=fixed_cost_shipping_price,is_free_shipping')
+    this._storeLogger.logTime('get product shipping information')
     const productEntity = this._productEntityFactory.create(response.data)
 
     return new ShopgateProductShipping(
