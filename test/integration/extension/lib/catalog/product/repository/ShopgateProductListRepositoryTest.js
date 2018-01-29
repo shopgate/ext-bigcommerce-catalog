bccd99ec156276192e48ce327e4ceb1b5a0bc6a5
Sofija Blazevski Rulani <sofija.blazevski@shopgate.com>
2018-01-29 11:57:16 +0100

    BIGC-631 Fixed getByProductIds integration test of ShopgateProductListRepository by introducing a regex instead of specifying exact featuredImageUrl.

--- test/integration/extension/lib/catalog/product/repository/ShopgateProductListRepositoryTest.js
+++ test/integration/extension/lib/catalog/product/repository/ShopgateProductListRepositoryTest.js
@@ -50,7 +50,6 @@ describe('ShopgateProductListRepository', function () {
           'active': true,
           'manufacturer': 'Anna',
           'name': '[Integration Test] Nutellabrot *Do not touch* ',
-          'featuredImageUrl': 'https://cdn3.bigcommerce.com/s-r5s844ad/products/112/images/345/Nutellabrot_1600x1067_8_24116__65804.1512562173.490.381.jpg?c=2',
           'price': {
             'unitPrice': 30,
             'unitPriceNet': 30,
@@ -65,7 +64,6 @@ describe('ShopgateProductListRepository', function () {
           'active': true,
           'manufacturer': 'Collette',
           'name': '[Integration Test][Sample] Collette, alligator clutch *Do not touch*',
-          'featuredImageUrl': 'https://cdn3.bigcommerce.com/s-r5s844ad/products/113/images/353/NI_colette_122__84607.1512562311.490.381.jpg?c=2',
           'price': {
             'unitPrice': 280,
             'unitPriceNet': 280,
@@ -78,7 +76,13 @@ describe('ShopgateProductListRepository', function () {
       ]
 
       return Promise.all([
-        response.should.be.fulfilled.and.eventually.have.property('products').containSubset(integrationTestProducts)
+        response.should.be.fulfilled
+          .and.eventually.have.property('products').containSubset(integrationTestProducts)
+          .and.then((products) => {
+            products.forEach((product) => {
+              product.featuredImageUrl.should.match(/^https:\/\/\w+.bigcommerce.com\/s-r5s844ad\/products\/.+$/)
+            })
+          })
       ])
     })
   })
