72d1225b24a0b9afced4f3f07343aa71a636b3cc
Rainer Skistims <rainer.skistims@shopgate.com>
2018-05-31 13:57:04 +0200

    BIGC-727 add cache logic to Wrapper

--- extension/lib/steps/BigCommerceWrapper.js
+++ extension/lib/steps/BigCommerceWrapper.js
@@ -57,11 +57,81 @@ class BigCommerceWrapper extends BigCommerce {
    * @return {Promise.<*>}
    */
   async request (type, path, data) {
+    const cacheKey = this.config.storeHash + path
+    if (type === 'get') {
+      const cacheData = await this._getCache(cacheKey)
+      if (cacheData !== undefined) {
+        return cacheData
+      }
+    }
+
     const requestId = this._timeStarted(path, type)
     const response = await super.request(type, path, data)
     this._timeFinished(requestId)
 
+    if (type === 'get') {
+      await this._setCache(cacheKey, response)
+    }
+
     return response
+  }
+
+  /**
+   * @param {string} key
+   * @private
+   */
+  async _getCache (key) {
+    if (this._cacheLifetime === undefined) {
+      return
+    }
+    const cacheData = await this._getfromStorage(key)
+    if (cacheData === undefined) {
+      return
+    }
+    const cachedTime = new Date(cacheData.time)
+    const invalidTime = new Date()
+    invalidTime.setSeconds(invalidTime.getSeconds() - this._cacheLifetime)
+    if (invalidTime < cachedTime) {
+      delete cacheData.time
+      return cacheData
+    }
+  }
+
+  /**
+   * @param {string} key
+   * @private
+   */
+  async _getfromStorage (key) {
+    return this._extensionStorage.get(key).then((cacheData) => {
+      return cacheData
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  /**
+   * @param {string} key
+   * @param {Object} cacheData
+   * @private
+   */
+  async _setCache (key, cacheData) {
+    if (this._cacheLifetime === undefined) {
+      return
+    }
+    cacheData.time = new Date()
+    await this._addToStorage(key, cacheData)
+  }
+
+  /**
+   * @param {string} key
+   * @param {Object} cacheData
+   * @private
+   */
+  async _addToStorage (key, cacheData) {
+    this._extensionStorage.set(key, cacheData).then(cacheData => {
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 
   /**
