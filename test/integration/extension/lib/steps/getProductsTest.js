f4df92ab6d21ca01d411d59c4465e73c906c572b
Konstantin Kiritsenko <k@componentk.com>
2018-06-10 21:29:00 -0500

    BIGC-747 adding product count to tests

--- test/integration/extension/lib/steps/getProductsTest.js
+++ test/integration/extension/lib/steps/getProductsTest.js
@@ -30,16 +30,19 @@ describe('getProducts step > ', () => {
     it('Should return all products when it is not a getHighlight pipeline call', async () => {
       const result = await getProductsPipeline(context, input)
       assert.equal(result.products.length, input.productIds.length)
+      assert.equal(result.totalProductCount, input.productIds.length)
     })
     it('Should return no products when highligh returning is skipped', async () => {
       input.skipHighlightLoading = true
       const result = await getProductsPipeline(context, input)
       assert.equal(result.products.length, 0)
+      assert.equal(result.totalProductCount, 0)
     })
     it('Should return only highlight products', async () => {
       input.skipHighlightLoading = false
       const result = await getProductsPipeline(context, input)
       assert.equal(result.products.length, 3)
+      assert.equal(result.totalProductCount, 3)
       assert.equal(result.products[0].highlight, true)
     })
   })
